d1d7aafc4bdbc2d7e54ea954d7a0d69fdca75b99
itachi_hitachi <yusenng88@gmail.com>
2023-02-02 22:37:32 +0800

    Chore: Importing data

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ file = open("bigmartsales.csv", "r")
 content = file.read()
 print(content)
 file.close()
+
+#importing data
+df = pd.read_csv('bigmartsales.csv')
+print(df.shape)
+
+#preparing the data for training
+#X =
