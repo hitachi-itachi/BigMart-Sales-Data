034ac44fc82690ea4f8d9e160c661ab431df846c
itachi_hitachi <yusenng88@gmail.com>
2023-02-04 21:40:40 +0800

    Chore: - Spilt train-test data - Predicted Sales of item based on item type

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pandas as pd
 import numpy as np
-import sklearn as sk
-
-
-#reading the data file
-file = open("bigmartsales.csv", "r")
-content = file.read()
-print(content)
-file.close()
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 #importing data
 df = pd.read_csv('bigmartsales.csv')
 print(df.shape)
 
 #preparing the data for training
-X = df['Item_Visibility', 'Item_MRP','Item_Outlet_Sales']
-Y = df['Item_Type', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type']
+X = df[['Item_Visibility', 'Item_MRP','Item_Outlet_Sales']]
+Y = df[['Item_Type']].values.reshape(-1, 1)
+
+print(df['Outlet_Type'].value_counts())
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.20, random_state=1)
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+# Make predictions on validation dataset
